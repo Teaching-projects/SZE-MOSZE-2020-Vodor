dc574d8cd441d66f183c9066589159b483c40c3c
Roland Borbély <roland.borbely12@gmail.com>
2020-10-15 19:22:35 +0200

    parseFromString function is now public

--- Parser.h
+++ Parser.h
@@ -5,9 +5,8 @@
 
 class Parser
 {
-private:
-    static const std::map <std::string, std::string> parseFromString(std::string inputString);
 public:
+    static const std::map <std::string, std::string> parseFromString(std::string inputString);
     static const std::map <std::string, std::string> parseJson(const std::string& json);
     static const std::map <std::string, std::string> parseJson(std::istream& jsonFile);
 };
