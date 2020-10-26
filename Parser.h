a9b3c12594201f785ca6c97eb51ccb218672bbbc
Roland Borbély <roland.borbely12@gmail.com>
2020-10-26 09:26:17 +0100

    comments added for documentation

--- Parser.h
+++ Parser.h
@@ -1,3 +1,20 @@
+/*!
+ * \class Parser
+ * 
+ * \brief Parser class
+ * 
+ * Ez az osztály a bemeneti fájlok parzolásáért felelős.
+ * 
+ * \author  Borbély Roland, Vitéz Marcell, Voznek Péter
+ * 
+ * \version 3.3
+ * 
+ * \date 2020/10/26 9:00
+ * 
+ * Created on 2020/10/26 9:00
+ */
+
+
 #include <map>
 #include <regex>
 #include <iostream>
@@ -6,7 +23,10 @@
 class Parser
 {
 public:
-    static const std::map <std::string, std::string> parseFromString(std::string inputString);
-    static const std::map <std::string, std::string> parseJson(const std::string& json);
-    static const std::map <std::string, std::string> parseJson(std::istream& jsonFile);
+    /// JSON formátumú string parzolására alkalmas függvény.
+    static const std::map <std::string, std::string> parseFromString(std::string inputString/** [in] karakterlánc, ami egy JSON formátumot tartalmaz */);
+    /// JSON fájl parzolására alkalmas függvény.
+    static const std::map <std::string, std::string> parseJson(const std::string& json/** [in] a JSON fájl elérési útja */);
+    /// JSON fájl parzolására alkalmas függvény.
+    static const std::map <std::string, std::string> parseJson(std::istream& jsonFile/** [in] a beolvasott JSON fájlt tartalmazó istream */);
 };
