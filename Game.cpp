c40bf2f32b63bfa7c3b54c576f9400120e9748d5
vit3z-marcell <70660483+vit3z-marcell@users.noreply.github.com>
2020-10-30 11:33:32 +0100

    unittests fix

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void Game::runGame() {
-    std::cout << u1->fight(u2)->getName() << " wins." << " Remaining hp: "<< u1->fight(u2)->getHP()<<".";
+    std::cout << u1->fight(u2)->getName() << " wins." << " Remaining hp: "<< u1->fight(u2)->getHp()<<".";
 }
 
 Game::~Game(){
