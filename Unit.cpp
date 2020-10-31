bd65fedcdc4c6bb60fffc11166d07e7c782fd3cb
Roland Borbély <roland.borbely12@gmail.com>
2020-10-31 20:19:07 +0100

    Unit updated

--- Unit.cpp
+++ Unit.cpp
@@ -1,23 +1,23 @@
-#include "Hero.h"
+#include "Unit.h"
 #include <cmath>
 #include <fstream>
 #include <map>
 
-Hero Hero::parse(const std::string& fname) {
+Unit Unit::parse(const std::string& fname) {
 	JSON returnedJSON = JSON::parseFromFile(fname);
 
 	if (returnedJSON.checkIfKeyExists("name") &&
 		returnedJSON.checkIfKeyExists("health_points") &&
 		returnedJSON.checkIfKeyExists("damage") &&
 		returnedJSON.checkIfKeyExists("attack_cooldown")) 
-			return Hero(returnedJSON.get<std::string>("name"), 
+			return Unit(returnedJSON.get<std::string>("name"), 
 					stoi(returnedJSON.get<std::string>("health_points")),
 					stoi(returnedJSON.get<std::string>("damage")),
 					stod(returnedJSON.get<std::string>("attack_cooldown")));
 	else throw "Incorrect attributes in " + fname + "!";
 }
 
-void Hero::levelup(){
+void Unit::levelup(){
 	while (b_xp >= 100){		
 		b_maxHp = round((b_maxHp*1.1));
 		b_hP = b_maxHp;
@@ -28,10 +28,10 @@ void Hero::levelup(){
 	}
 }
 
-void Hero::getHitBy(Monster *other) {
-	if (b_hP - other->getDmg() > 0) {
-		other->b_xp += other->getDmg();
-		b_hP -= other->getDmg();
+void Unit::getHitBy(Unit* other) {
+	if (b_hP - other->getDamage() > 0) {
+		other->b_xp += other->getDamage();
+		b_hP -= other->getDamage();
 	}
 	else { 
 		other->b_xp += b_hP;
@@ -40,53 +40,6 @@ void Hero::getHitBy(Monster *other) {
 	other->levelup();
 }
 
-bool Hero::isAlive() const {
+bool Unit::isAlive() const {
 	return b_hP > 0;
-}
-
-void Hero::fightTilDeath(Monster *other) {
-
-	other->getHitBy(this);
-	double acdthis = this->getAcd();
-	double acdother = other->getAcd();
-	Hero* last = this;
-
-	while(this->isAlive() && other->isAlive())
-	{
-		if(acdthis == acdother)
-		{
-			if (last == this)
-			{
-				other->getHitBy(this);
-				if(other->isAlive())
-					this->getHitBy(other);
-				acdthis = this->getAcd();
-				acdother = other->getAcd();
-				last = other;
-			}
-			else
-			{
-				this->getHitBy(other);
-				if(this->isAlive())
-					other->getHitBy(this);
-				acdthis = this->getAcd();
-				acdother = other->getAcd();
-				last = this;
-			}	
-		}
-		else if((acdthis - acdother) < 0)
-		{
-			other->getHitBy(this);
-			acdother -= acdthis;
-			acdthis = this->getAcd();
-			last = this;
-		}
-		else 
-		{
-			this->getHitBy(other);
-			acdthis -= acdother;
-			acdother = other->getAcd();
-			last = other;
-		}		
-	}
 }
--- Unit.h
+++ Unit.h
@@ -18,14 +18,12 @@
 
 #include <string>
 #include "JSON.h"
-#include "Monster.h"
 
-class Hero
+class Unit
 {
 public:
-	/// A fight függvény levezényli a csatát és a végén kiírja a győztest. 
-	void fightTilDeath(Hero *other /** [in] ellenfél karakter Hero */);
-	/*! \brief Hero konstruktor
+
+	/*! \brief Unit konstruktor
  	*         
  	*  
  	*  Beállítja a karakterek adatait a paraméterek alapján.
@@ -35,18 +33,30 @@ public:
 	*  \param dmg [in] karakter támadási ereje
 	*  \param acd [in] karakter támadási ideje
  	*/
-	Hero(const std::string& name, int hP, int dmg, double acd) : b_name(name), b_hP(hP), b_dmg(dmg), b_acd(acd), b_maxHp(hP), b_xp(0), b_level(1) {}
+	Unit(const std::string& name, int hP, int dmg, double acd) : b_name(name), b_hP(hP), b_dmg(dmg), b_acd(acd), b_maxHp(hP), b_xp(0), b_level(1) {}
 	/// Ez a függvény a .json file parse-olja. 
-	static Hero parse(const std::string& fname/** [in] elérési út */);
+	static Unit parse(const std::string& fname/** [in] elérési út */);
 	/// Ez a függvény visszaadja a karakter életerejét.
-	int getHP() const { return b_hP; }
+	int getHealthPoints() const { return b_hP; }
+	/// Ez a függvény visszaadja a karakter maximális életerejét.
+	int getMaxHealthPoints() const {return b_maxHp; }
 	/// Ez a függvény visszaadja a karakter nevét.
 	std::string getName() const { return b_name; }
 	/// Ez a függvény visszaadja a karakter meghalt-e.
 	bool isAlive() const;
 	/// Ez a függvény visszaadja a karakter szintjét.
 	int getLevel() const { return b_level; }
-private:
+
+	/// Ez a függvény ellenőrzi/végzi a szintlépést.
+	void levelup();
+	/// Ez a függvény sebzést oszt ki a karakterre.
+	void getHitBy(Unit *other /** [in] ellenfél karakter */ );
+	/// Ez a függvény visszaadja a karakter támadási erejét.
+	int getDamage() const { return b_dmg; }
+	/// Ez a függvény visszaadja a karakter támadási idejét.
+	double getAttackCoolDown() const { return b_acd; } 
+
+protected:
 	const std::string b_name;  ///< A karakter neve. 
 	int b_hP; ///< A karakter életereje.
 	int b_dmg; ///< A karakter támadási ereje.
@@ -54,13 +64,4 @@ private:
 	int b_maxHp; ///< Karakter maximális életereje.
 	int b_xp; ///< Karakter fejlődési pont.
 	int b_level; ///< Karakter szintje.
-
-	/// Ez a függvény ellenőrzi/végzi a szintlépést.
-	void levelup();
-	/// Ez a függvény sebzést oszt ki a karakterre.
-	void getHitBy(Monster *other /** [in] ellenfél karakter */ );
-	/// Ez a függvény visszaadja a karakter támadási erejét.
-	int getDmg() const { return b_dmg; }
-	/// Ez a függvény visszaadja a karakter támadási idejét.
-	double getAcd() const { return b_acd; } 
 };
