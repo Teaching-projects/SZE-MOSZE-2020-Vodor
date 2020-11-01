1b6ea5951c0ec01957c778e1d248eef70a431a26
Roland Borbély <roland.borbely12@gmail.com>
2020-11-01 12:31:48 +0100

    testing

--- Unit.cpp
+++ Unit.cpp
@@ -4,3 +4,12 @@
 bool Unit::isAlive() const {
 	return b_hP > 0;
 }
+
+void Unit::getHitBy(Unit* other){
+	if (b_hP - other->getDamage() > 0) {
+		b_hP -= other->getDamage();
+	}
+	else { 
+		b_hP = 0;
+	}
+}
