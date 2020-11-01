5d914bd4ac1013ae909441be786d9d316fa6e481
Roland Borbély <roland.borbely12@gmail.com>
2020-11-01 19:28:07 +0100

    diff tests updated for scenarios

--- run_test.sh
+++ run_test.sh
@@ -5,8 +5,8 @@ then
     rm output.txt
 fi
 
-for file1 in test/units/*.json; do
-    for file2 in test/units/*.json; do
+for file1 in test/scenarios/*.json; do
+    for file2 in test/scenarios/*.json; do
         if [ $file1 != $file2 ]
         then
             ./a.out $file1 $file2 >> output.txt
