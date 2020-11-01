4d76cd85ad8a4434c53f4f82755a3d2dc707d888
rolandborbely <43845708+rolandborbely@users.noreply.github.com>
2020-11-01 08:42:03 +0100

    indent fix

--- Hero.cpp
+++ Hero.cpp
@@ -48,13 +48,12 @@ void Hero::fightTilDeath(Unit other) {
 }
 
 Hero Hero::parse(const std::string& fname) {
-    std::vector <std::string, std::string> keysNeeded = 
-    {"experience_per_level","health_point_bonus_per_level", "damage_bonus_per_level",
-     "cooldown_multiplier_per_level","name", "healh_points", "damage", "attack_cooldown"};
+	std::vector <std::string, std::string> keysNeeded = {"experience_per_level","health_point_bonus_per_level", "damage_bonus_per_level",
+							 "cooldown_multiplier_per_level","name", "healh_points", "damage", "attack_cooldown"};
 	JSON returnedJSON = JSON::parseFromFile(fname);
-    bool okay = true;
-    for (auto key : keysNeeded)
-        if(!returnedJSON.checkIfKeyExists(key));
+    	bool okay = true;
+    	for (auto key : keysNeeded)
+        	if(!returnedJSON.checkIfKeyExists(key));
 			okay = false;
     
 	if (okay) 
@@ -62,9 +61,9 @@ Hero Hero::parse(const std::string& fname) {
 			stoi(returnedJSON.get<std::string>("health_points")),
 			stoi(returnedJSON.get<std::string>("damage")),
 			stod(returnedJSON.get<std::string>("attack_cooldown")),
-            stoi(returnedJSON.get<std::string>("experience_per_level")),
-            stoi(returnedJSON.get<std::string>("health_point_bonus_per_level")),
-            stoi(returnedJSON.get<std::string>("damage_bonus_per_level")),
-            stod(returnedJSON.get<std::string>("cooldown_multiplier_per_level")));
+			stoi(returnedJSON.get<std::string>("experience_per_level")),
+			stoi(returnedJSON.get<std::string>("health_point_bonus_per_level")),
+			stoi(returnedJSON.get<std::string>("damage_bonus_per_level")),
+			stod(returnedJSON.get<std::string>("cooldown_multiplier_per_level")));
 	else throw "Incorrect attributes in " + fname + "!";
 }
