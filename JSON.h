7a38b95880016985aa0c9b2fbcf9531aa5b56c1d
rolandborbely <43845708+rolandborbely@users.noreply.github.com>
2020-11-05 10:27:37 +0100

    doc updates

--- JSON.h
+++ JSON.h
@@ -39,23 +39,39 @@ public:
 	*  \param data [in] adat.
  	*/
     JSON(jsonData data) : b_data(data){}
-    /// Ez a függvény a String értéket parse-olja.
-    static const JSON parseFromString(std::string inputString);
-    /// Ez a függvény a Fájlt parse-olja.
-    static const JSON parseFromFile(const std::string& json);
-    /// Ez a függvény a .json file parse-olja.
-    static const JSON parseJson(std::istream& jsonFile);
+    /// JSON fájlból beolvasott karakterlánc parzolását végző függvény.
+    /// Hibát dob, amennyiben hiányos, vagy sérült a fájl. Pl.: hiányoznak kulcsok adott párokból, vagy nincsenek meg a kezdő, és/vagy záró karakterek
+    /*!
+    \return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
+    */
+    static const JSON parseFromString(std::string inputString /** [in] JSON formátumú karakterlánc */);
+    /// Egy fájl elérési útja alapján parzoló függvény. Működése során meghívja a istream-et váró overloadolt függvényt.
+    /// Hibát dob, amennyiben a megadott elérési úton a fájl nem található.
+    /*!
+    \return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
+    */
+    static const JSON parseFromFile(const std::string& json/** [in] a JSON fájl elérési útja */);
+    /// Egy korábban beolvasott JSON fájl parzolását végző függvény. Működése során meghívja a parseFromString() függvényt.
+    /*!
+    \return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
+    */
+    static const JSON parseJson(std::istream& jsonFile/** [in] a beolvasott JSON fájlt tartalmazó istream */);
     /// Ez a függvény vizsgálja adott key benne van-e mapben.
-    const int count(const std::string& key);
+    const int count(const std::string& key/** [in] kulcs*/);
     /// Ez a függvény visszaad egy valuet a key alapján.
-    template <typename T> T get(const std::string& key){
+	///Hibát dob, amennyiben az adott kulcs nincs a mapben.
+    template <typename T> T get(const std::string& key/** [in] kulcs*/){
         if (!count(key)) throw ParseException("Key does not exist in map!");
         else return std::get<T>(b_data[key]);
     }
 
     class ParseException : public std::runtime_error{
     public:
-         /// Ez a függvény hiba üzentet ad.
+        /*! \brief ParseException konstruktor
+ 	*         
+ 	*  
+	*  \param data [in] hibaüzenet.
+ 	*/
         ParseException(const std::string& errMsg) : std::runtime_error(errMsg){}
     };
 };
