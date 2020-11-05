4427652da67d8a24ad508e53dead9c43d8138a35
rolandborbely <43845708+rolandborbely@users.noreply.github.com>
2020-11-05 10:31:41 +0100

    indent

--- JSON.h
+++ JSON.h
@@ -31,49 +31,49 @@ using jsonData = std::map <std::string, std::variant<std::string, int, double>>;
 class JSON
 {
 private:
-    jsonData b_data; ///< Adatoknak létrehozott map.
+	jsonData b_data; ///< Adatoknak létrehozott map.
 public:
-    /*! \brief JSON konstruktor
+    	/*! \brief JSON konstruktor
  	*         
  	*  
 	*  \param data [in] adat.
  	*/
-    JSON(jsonData data) : b_data(data){}
-    /// JSON fájlból beolvasott karakterlánc parzolását végző függvény.
-    /// Hibát dob, amennyiben hiányos, vagy sérült a fájl. Pl.: hiányoznak kulcsok adott párokból, vagy nincsenek meg a kezdő, és/vagy záró karakterek
-    /*!
-    \return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
-    */
-    static const JSON parseFromString(std::string inputString /** [in] JSON formátumú karakterlánc */);
-    /// Egy fájl elérési útja alapján parzoló függvény. Működése során meghívja a istream-et váró overloadolt függvényt.
-    /// Hibát dob, amennyiben a megadott elérési úton a fájl nem található.
-    /*!
-    \return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
-    */
-    static const JSON parseFromFile(const std::string& json/** [in] a JSON fájl elérési útja */);
-    /// Egy korábban beolvasott JSON fájl parzolását végző függvény. Működése során meghívja a parseFromString() függvényt.
-    /*!
-    \return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
-    */
-    static const JSON parseJson(std::istream& jsonFile/** [in] a beolvasott JSON fájlt tartalmazó istream */);
-    /// Ez a függvény vizsgálja adott key benne van-e mapben.
-    const int count(const std::string& key/** [in] kulcs*/);
-    /// Ez a függvény visszaad egy valuet a key alapján.
+    	JSON(jsonData data) : b_data(data){}
+    	/// JSON fájlból beolvasott karakterlánc parzolását végző függvény.
+    	/// Hibát dob, amennyiben hiányos, vagy sérült a fájl. Pl.: hiányoznak kulcsok adott párokból, vagy nincsenek meg a kezdő, és/vagy záró karakterek
+    	/*!
+    	\return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
+    	*/
+    	static const JSON parseFromString(std::string inputString /** [in] JSON formátumú karakterlánc */);
+    	/// Egy fájl elérési útja alapján parzoló függvény. Működése során meghívja a istream-et váró overloadolt függvényt.
+    	/// Hibát dob, amennyiben a megadott elérési úton a fájl nem található.
+    	/*!
+    	\return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
+    	*/
+    	static const JSON parseFromFile(const std::string& json/** [in] a JSON fájl elérési útja */);
+    	/// Egy korábban beolvasott JSON fájl parzolását végző függvény. Működése során meghívja a parseFromString() függvényt.
+    	/*!
+    	\return Visszaadja a JSON fájlból beolvasott értékeket egy mapben.
+   	 */
+    	static const JSON parseJson(std::istream& jsonFile/** [in] a beolvasott JSON fájlt tartalmazó istream */);
+    	/// Ez a függvény vizsgálja adott key benne van-e mapben.
+    	const int count(const std::string& key/** [in] kulcs*/);
+    	/// Ez a függvény visszaad egy valuet a key alapján.
 	///Hibát dob, amennyiben az adott kulcs nincs a mapben.
-    template <typename T> T get(const std::string& key/** [in] kulcs*/){
-        if (!count(key)) throw ParseException("Key does not exist in map!");
-        else return std::get<T>(b_data[key]);
-    }
+   	template <typename T> T get(const std::string& key/** [in] kulcs*/){
+        	if (!count(key)) throw ParseException("Key does not exist in map!");
+        	else return std::get<T>(b_data[key]);
+    	}
 
-    class ParseException : public std::runtime_error{
-    public:
-        /*! \brief ParseException konstruktor
- 	*         
- 	*  
-	*  \param data [in] hibaüzenet.
- 	*/
-        ParseException(const std::string& errMsg) : std::runtime_error(errMsg){}
-    };
+    	class ParseException : public std::runtime_error{
+    	public:
+        	/*! \brief ParseException konstruktor
+ 		*         
+ 		*  
+		*  \param data [in] hibaüzenet.
+ 		*/
+        	ParseException(const std::string& errMsg) : std::runtime_error(errMsg){}
+    	};
 };
 
 #endif
