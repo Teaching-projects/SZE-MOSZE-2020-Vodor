e5dca6cfa8c9457a5c0ec4c1e89b926765ec0b07
Roland Borbély <roland.borbely12@gmail.com>
2020-11-30 15:24:58 +0100

    main updated

--- main.cpp
+++ main.cpp
@@ -32,108 +32,52 @@ void bad_exit(int exitcode){
 }
  
 int main(int argc, char** argv){
-    if(argc==1){
-        Game thegame{};
-        std::string input, filename;
-        int x,y;
-        do{
-            std::cout<<"What would you like to do? Possible commands: setmap, puthero, putmonster, run, exit\n";
-            std::cin >> input;
-            if(input == "setmap")
-            {
-                std::cout<<"Add the file path for the map!\n";
-                std::cin >> filename;
-                try
-                {
-                    if (!std::filesystem::exists(filename)) bad_exit(2);
-                    thegame.setMap(Map{filename});
-                    std::cout<<"The selected map: "<<std::endl;
-                    thegame.printMap();
-                }
-                catch(const Game::AlreadyHasUnitsException& e) { bad_exit(6);}
-                catch(const Game::GameAlreadyStartedException& e) {bad_exit(7);}
-            }
-            if(input == "puthero")
-            {
-                std::cout<<"Add the hero file path and coordinates with a whitespace character separated (eg: filename x y)!\n";
-                std::cin >> filename >> x >> y;
-                try
-                {
-                    if (!std::filesystem::exists(filename)) bad_exit(2);
-                    if(std::cin.good()) bad_exit(-1);
-                    thegame.putHero(Hero{Hero::parse(filename)},x,y);
-                }
-                catch(const Game::GameAlreadyStartedException& e){bad_exit(7);}
-                catch(const Game::OccupiedException& e){bad_exit(8);}
-                catch(const Map::WrongIndexException& e){bad_exit(9);}
-            }
-            if(input == "putmonster")
-            {
-                std::cout<<"Add the monster file path and coordinates with a whitespace character separated (eg: filename x y)!\n";
-                std::cin >> filename >> x >> y;
-                try
-                {
-                    if (!std::filesystem::exists(filename)) bad_exit(2);
-                    if(!std::cin.good()) bad_exit(-1);
-                    thegame.putMonster(Monster{Monster::parse(filename)},x,y);
-                }
-                catch(const Game::OccupiedException& e){bad_exit(8);}
-                catch(const Map::WrongIndexException& e){bad_exit(9);}
-            }
-            if("run" == input){
-                try
-                {
-                    thegame.run();
-                }
-                catch(const Game::NotInitializedException& e) {bad_exit(5);}
-                std::cout<<"Would you like to play another round? Note: you have to set a new Hero for a new game. (Y/N)";
-                std::cin >> input;
-                if(input == "N")
-                    input = "exit";
-            }
-        }while(input != "exit");
-    }
-
-    else if (argc == 2){
+    if (argc!=2) bad_exit(1);
+    else{
         if (!std::filesystem::exists(argv[1])) bad_exit(2);
+        int herox, heroy, monsterx, monstery, spacePos;
         std::string hero_file;
-        std::list<std::string> monster_files;
-        int x,y;
         std::string mapFile;
+        std::list<std::string> monster_files;
+        std::list<std::string> monster_positions;
         try {
             JSON scenario = JSON::parseFromFile(argv[1]); 
-            if (!(scenario.count("hero")&&scenario.count("monsters"))) bad_exit(3);
+            if (!(scenario.count("hero")&&scenario.count("monsters"))&&scenario.count("hero_position")&&scenario.count("positions")&&scenario.count("map")) bad_exit(3);
             else {
                 hero_file=scenario.get<std::string>("hero");
+                mapFile = scenario.get<std::string>("map");
+                std::string hero_position = scenario.get<std::string>("hero_position");
+                spacePos = hero_position.find(" ");
+                herox = std::stoi(hero_position.substr(0,spacePos));
+                heroy = std::stoi(hero_position.substr(spacePos));
                 JSON::list monster_file_list=scenario.get<JSON::list>("monsters");
+                JSON::list monster_positions_list = scenario.get<JSON::list>("positions");
                 for(auto monster_file : monster_file_list)
                     monster_files.push_back(std::get<std::string>(monster_file));
+                for (auto monster_position : monster_positions_list)
+                    monster_positions.push_back(std::get<std::string>(monster_position));
             }
         } catch (const JSON::ParseException& e) {bad_exit(4);}
-        
-        std::cout<<"Add the file path for the map!\n";
-        std::cin >> mapFile;
+
         if (!std::filesystem::exists(mapFile)) bad_exit(2);
         Game thegame{mapFile};
-        std::cout<<"The selected map: "<<std::endl;
-        thegame.printMap();
-        std::cout <<"Add the coordinates for the Hero("<<hero_file<<") (x,y): ";
-        std::cin >> x >> y;
         try
         {
-            if(!std::cin.good()) bad_exit(-1);
-            thegame.putHero(Hero::parse(hero_file),x,y);
+            thegame.putHero(Hero::parse(hero_file),herox,heroy);
         }
         catch(const Game::GameAlreadyStartedException& e){bad_exit(7);}
         catch(const Game::OccupiedException& e){bad_exit(8);}
         catch(const Map::WrongIndexException& e){bad_exit(9);}
-
-        for (const auto& monster_file : monster_files){
-            std::cout<<"Add the coordinates for Monster("<<monster_file<<") (x, y): ";
-            std::cin >> x >> y;
+        
+        while (!monster_files.empty())
+        {
             try{
-                if(!std::cin.good()) bad_exit(-1);
-                    thegame.putMonster(Monster::parse(monster_file),x,y);
+                monsterx = std::stoi(monster_positions.front());
+                monster_positions.pop_front();
+                monstery = std::stoi(monster_positions.front());
+                thegame.putMonster(Monster::parse(monster_files.front()),monsterx,monstery);
+                monster_files.pop_front();
+                monster_positions.pop_front();
             }
             catch(const Game::OccupiedException& e){bad_exit(8);}
             catch(const Map::WrongIndexException& e){bad_exit(9);}
@@ -143,8 +87,6 @@ int main(int argc, char** argv){
             thegame.run();
         }
         catch(const Game::NotInitializedException& e) {bad_exit(5);}
-    }
-    else bad_exit(1);
-    
+    }    
     return 0;
 }
