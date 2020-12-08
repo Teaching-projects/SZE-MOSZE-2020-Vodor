c1ea665e6fa6e095663f1ffc95ada2f5cb5f6178
Roland Borbély <roland.borbely12@gmail.com>
2020-12-08 14:57:50 +0100

    function moved

--- Hero.h
+++ Hero.h
@@ -38,9 +38,6 @@ private:
 	int b_light_radius_bonus_per_level; ///< Hős látótávolsága növekedése szintenként.
 
 public:
-    /// A fightTilDeath függvény levezényli a csatát. 
-	void fightTilDeath(Monster& other /** [in] ellenfél karakter */);
-
      /*! \brief Hero konstruktor
  	*         
  	*  
@@ -79,8 +76,11 @@ public:
 		b_defense_bonus_per_level(defense_bonus_per_level),
 		b_light_radius(light_radius),
 		b_light_radius_bonus_per_level(light_radius_bonus_per_level){} 
+
     /// Ez a függvény parse-olja a megadott fájlt. 
-    static Hero parse(const std::string& fname/** [in] elérési út */);   
+    static Hero parse(const std::string& fname/** [in] elérési út */); 
+	/// A fightTilDeath függvény levezényli a csatát. 
+	void fightTilDeath(Monster& other /** [in] ellenfél karakter */);  
     /// Ez a függvény visszaadja a Hős szintjét.
 	int getLevel() const { return b_level; }
     /// Ez a függvény visszaadja a Hős maximális életerejét.
