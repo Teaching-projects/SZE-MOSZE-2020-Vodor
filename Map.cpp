525b3d57609b9de053b4a89857754ee407428371
Roland Borbély <roland.borbely12@gmail.com>
2020-12-08 20:33:40 +0100

    missing const

--- Map.cpp
+++ Map.cpp
@@ -18,7 +18,7 @@ Map::type Map::get(unsigned int x, unsigned int y) const{
     
 }
 
-int Map::getMaxLength(){
+int Map::getMaxLength() const{
     int maxLength = 0;
     for (int i = 0; i < (int) map.size(); i++)
         if ((int) map[i].length()>maxLength)
@@ -32,7 +32,7 @@ MarkedMap::MarkedMap(const std::string& filename){
     std::string line;
     while (getline(mapFile, line))
         map.push_back(line);
-    
+
     mapFile.close();
 }
 
@@ -54,5 +54,6 @@ std::list<std::pair<int,int>> MarkedMap::getMonsterPositions(char c) const{
             if(map[i][j] == c)
                 monsterPositions.push_back(std::make_pair(j,i));
     
+    if(monsterPositions.empty()) throw std::runtime_error(c+ " monster is not on the map.");
     else return monsterPositions;
 }
